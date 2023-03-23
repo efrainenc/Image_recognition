72cab829b8535630c775c706ffd0e1177bc385c8
efrainenc <efraine387@gmail.com>
2023-03-23 14:57:40 -0600

    create snowpark session function

--- image_upload.py
+++ image_upload.py
@@ -1,12 +1,27 @@
 import streamlit as st
+import pandas as pd
+from snowflake.snowpark import Session
 
-# uses Streamlit's "st.file_uploader()" to allow the user to upload an image file.
+# Function that create's a new or get existing Snowpark session
+def create_session():
+    if "snowpark_session" not in st.session_state:
+        session = Session.builder.configs(json.load(open("connection.json"))).create()
+        st.session_state['snowpark_session'] = session
+    else:
+        session = st.session_state['snowpark_session']
+    return session
+
+# Calling the Snowpark session function
+session = create_session()
+
+# Uses Streamlit's "st.file_uploader()" to allow the user to upload an image file.
 uploaded_file = st.file_uploader("Choose an image file", accept_multiple_files=False, label_visibility='hidden')
+# Once uploaded it then vv
 if uploaded_file is not None:
-  # Convert image base64 string into hex 
+  # Converts image base64 string into hex 
   bytes_data_in_hex = uploaded_file.getvalue().hex()
 
-  # Generate new image file name
+  # Generates new image file name
   file_name = 'img_' + str(uuid.uuid4())
 
   # Write image data in Snowflake table
